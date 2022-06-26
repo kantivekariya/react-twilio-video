57bfdddb603d9c21e89ddbc3e696e89038bd41d1
kantivekariya <kantivekariya242@gmail.com>
2022-06-26 04:36:01 -0700

    added Participant

--- twilio-client/src/component/video-chat.tsx
+++ twilio-client/src/component/video-chat.tsx
@@ -1,54 +1,105 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+// @ts-ignore
+import Video from "twilio-video";
 import Lobby from "./lobby";
+import Room from "./room";
 
 const VideoChat = () => {
   const [username, setUsername] = useState("");
   const [roomName, setRoomName] = useState("");
   const [token, setToken] = useState(null);
+  const [room, setRoom] = useState<any>(null);
+  const [connecting, setConnecting] = useState(false);
 
-  const handleUsernameChange = useCallback((event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setUsername(event.target.value);
-  }, []);
+  const handleUsernameChange = useCallback(
+    (event: { target: { value: React.SetStateAction<string> } }) => {
+      setUsername(event.target.value);
+    },
+    []
+  );
 
-  const handleRoomNameChange = useCallback((event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setRoomName(event.target.value);
-  }, []);
+  const handleRoomNameChange = useCallback(
+    (event: { target: { value: React.SetStateAction<string> } }) => {
+      setRoomName(event.target.value);
+    },
+    []
+  );
 
-  const handleSubmit = useCallback(async (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    const data = await fetch('http://localhost:5000/rooms/join-room', {
-      method: 'POST',
-      body: JSON.stringify({
-        roomName: roomName
-      }),
-      headers: {
-        'Content-Type': 'application/json'
+  const handleSubmit = useCallback(
+    async (event: { preventDefault: () => void }) => {
+      event.preventDefault();
+      const data = await fetch("http://localhost:5000/rooms/join-room", {
+        method: "POST",
+        body: JSON.stringify({
+          roomName: roomName,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+      Video.connect(data.token, {
+        name: roomName,
+      })
+        .then((room: any) => {
+          setConnecting(false);
+          setRoom(room);
+        })
+        .catch((err: any) => {
+          console.error(err);
+          setConnecting(false);
+        });
+    },
+    [username, roomName]
+  );
+
+  const handleLogout = useCallback(() => {
+    setRoom((prevRoom: { localParticipant: { tracks: { track: { stop: () => void; }; }[]; }; disconnect: () => void; }) => {
+      if (prevRoom) {
+        prevRoom.localParticipant.tracks.forEach((trackPub: { track: { stop: () => void; }; }) => {
+          trackPub.track.stop();
+        });
+        prevRoom.disconnect();
       }
-    }).then(res => res.json());
-    setToken(data?.token)
-  }, [username, roomName]);
-
-  const handleLogout = useCallback((event: any) => {
-    setToken(null);
+      return null;
+    });
   }, []);
+
+  useEffect(() => {
+    if (room) {
+      const tidyUp = (event:any) => {
+        if (event.persisted) {
+          return;
+        }
+        if (room) {
+          handleLogout();
+        }
+      };
+      window.addEventListener("pagehide", tidyUp);
+      window.addEventListener("beforeunload", tidyUp);
+      return () => {
+        window.removeEventListener("pagehide", tidyUp);
+        window.removeEventListener("beforeunload", tidyUp);
+      };
+    }
+  }, [room, handleLogout]);
 
   let render;
-  if (token) {
+  if (room) {
     render = (
-      <div>
-        <p>Username: {username}</p>
-        <p>Room name: {roomName}</p>
-        <p>Token: {token}</p>
-      </div>
+      <Room
+        roomName={roomName}
+        handleLogout={handleLogout}
+        room={room}
+      />
     );
   } else {
     render = (
       <Lobby
-         username={username}
-         roomName={roomName}
-         handleUsernameChange={handleUsernameChange}
-         handleRoomNameChange={handleRoomNameChange}
-         handleSubmit={handleSubmit}
+        username={username}
+        roomName={roomName}
+        handleUsernameChange={handleUsernameChange}
+        handleRoomNameChange={handleRoomNameChange}
+        handleSubmit={handleSubmit}
       />
     );
   }
